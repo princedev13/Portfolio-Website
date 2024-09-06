c5dc608300f04bac34c46829bfca9f8b45143ff1
princedev13 <princefoster73@gmail.com>
2024-09-05 22:31:51 -0400

    added description for first dead egos website

--- src/projects.jsx
+++ src/projects.jsx
@@ -6,11 +6,15 @@ function Projects () {
                 <h2 class="projects-header">Projects</h2>
                 <div class="project-item"></div>
                 <img class="project-img" src="src/assets/dead egos screenshot 1.png"></img>
-                <h3>E-Commerce Website</h3>
+                <h2>E-Commerce Website</h2>
             </div>
 
             <div class="projects-desc1">
-                <h2>description</h2>
+                <h3>-Developed a full-stack web application
+                    -Integrated Stripe API
+                    -Designed and implemented an interactive character creator
+                    -Configured a robust database to manage user accounts and store purchase history for an enhanced user experience and data tracking.
+                </h3>
             </div>
         </>
     );
